5c8ab7daf2fd163cb0385e5a0dc81ed71eb2fffe
Rfluid <ruy.vieiraneto@gmail.com>
2025-02-18 23:51:27 -0300

    Fix login pages

--- src/app/(authentication)/login/components/user-auth-form.tsx
+++ src/app/(authentication)/login/components/user-auth-form.tsx
@@ -1,31 +1,29 @@
-"use client"
+"use client";
 
-import * as React from "react"
+import * as React from "react";
 
-import { cn, validateEmail } from "@/lib/utils"
-import { Label } from "@/components/ui/label"
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
-import { Icons } from "@/components/icons"
-import { UserRecoveryForm } from "./user-recovery-form"
-import { fadeBackgroundControllStore } from "@/zustand-store/fade-backgroung"
-import { login } from "@/services/authService"
-import { useRouter } from "next/navigation"
-import { AlertDialogComponent } from "@/components/alertComponent"
-import { AlertToastComponent } from "../../../../components/alert"
+import { cn, validateEmail } from "@/lib/utils";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { UserRecoveryForm } from "./user-recovery-form";
+import { fadeBackgroundControllStore } from "@/zustand-store/fade-backgroung";
+import { login } from "@/services/authService";
+import { useRouter } from "next/navigation";
+import { AlertToastComponent } from "../../../../components/alert";
 
-interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> { }
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
-    const { setIsAlertDialogOpen } = fadeBackgroundControllStore()
-    const [isLoading, setIsLoading] = React.useState<boolean>(false)
-    const [showPassword, setShowPassword] = React.useState<boolean>(false)
-    const [showDialog, setShowDialog] = React.useState<boolean>(false)
+    const { setIsAlertDialogOpen } = fadeBackgroundControllStore();
+    const [isLoading, setIsLoading] = React.useState<boolean>(false);
+    const [showPassword, setShowPassword] = React.useState<boolean>(false);
+    const [showDialog, setShowDialog] = React.useState<boolean>(false);
 
-    const [email, setEmail] = React.useState<string>('');
-    const [password, setPassword] = React.useState<string>('');
+    const [email, setEmail] = React.useState<string>("");
+    const [password, setPassword] = React.useState<string>("");
 
-    const router = useRouter()
+    const router = useRouter();
 
     const handleAction = () => {
         console.log("Action confirmed!");
@@ -38,29 +36,34 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
     };
 
     async function onSubmitLogin(event: React.SyntheticEvent) {
-        event.preventDefault()
-        setIsLoading(true)
+        event.preventDefault();
+        setIsLoading(true);
         await login({ email, password })
             .then(() => {
-                setIsLoading(false)
-                router.replace('/')
-            }).catch(e => {
-                setIsLoading(false)
-                setShowDialog(true);
+                setIsLoading(false);
+                router.replace("/");
             })
+            .catch((e) => {
+                setIsLoading(false);
+                setShowDialog(true);
+            });
     }
 
     return (
         <>
             <AlertToastComponent
-                helperText={'E-mail e/ou senha inválidos! Por favor verifique seus dados e tente novamente.'}
-                className={!showDialog ? 'hidden' : ''}
+                helperText={
+                    "E-mail e/ou senha inválidos! Por favor verifique seus dados e tente novamente."
+                }
+                className={!showDialog ? "hidden" : ""}
             />
             <div className={cn("grid", className)} {...props}>
-
                 <div className="grid gap-10">
                     <div className="grid gap-4">
-                        <Label className="text-purple-700 font-semibold text-[16px]" htmlFor="email">
+                        <Label
+                            className="text-purple-700 font-semibold text-[16px]"
+                            htmlFor="email"
+                        >
                             E-mail
                         </Label>
                         <Input
@@ -72,12 +75,21 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
                             autoCorrect="off"
                             disabled={isLoading}
                             onChange={(e) => setEmail(e.target.value)}
-                            helperText={email && !validateEmail(email) ? 'Formato de e-mail inválido. Por favor verifique o campo e tente novamente.' : ''}
-                            error={email && !validateEmail(email) ? true : false}
+                            helperText={
+                                email && !validateEmail(email)
+                                    ? "Formato de e-mail inválido. Por favor verifique o campo e tente novamente."
+                                    : ""
+                            }
+                            error={
+                                email && !validateEmail(email) ? true : false
+                            }
                         />
                     </div>
                     <div className="grid gap-4">
-                        <Label className="text-purple-700 font-semibold text-[16px]" htmlFor="password">
+                        <Label
+                            className="text-purple-700 font-semibold text-[16px]"
+                            htmlFor="password"
+                        >
                             Senha
                         </Label>
                         <Input
@@ -91,21 +103,31 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
                             onChange={(e) => setPassword(e.target.value)}
                             passwordIcon
                             showPassword={showPassword}
-                            setShowPassword={() => setShowPassword(!showPassword)}
+                            setShowPassword={() =>
+                                setShowPassword(!showPassword)
+                            }
                         />
                         <div className="flex justify-end w-full">
-                            <UserRecoveryForm onOpenChange={(open) => setIsAlertDialogOpen(open)} />
+                            <UserRecoveryForm
+                                onOpenChange={(open) =>
+                                    setIsAlertDialogOpen(open)
+                                }
+                            />
                         </div>
                     </div>
                     <Button
-                        disabled={isLoading || !validateEmail(email) || password === ''}
+                        disabled={
+                            isLoading ||
+                            !validateEmail(email) ||
+                            password === ""
+                        }
                         isLoading={isLoading}
                         label="Entrar"
                         onClick={onSubmitLogin}
                     />
                 </div>
-
             </div>
         </>
-    )
+    );
 }
+
--- src/app/(authentication)/login/components/user-recovery-form.tsx
+++ src/app/(authentication)/login/components/user-recovery-form.tsx
@@ -1,48 +1,61 @@
-"use client"
+"use client";
 
-import * as React from "react"
+import * as React from "react";
 
-import { Button } from "@/components/ui/button"
-import { AlertDialog, AlertDialogCancel, AlertDialogContent, AlertDialogFooter, AlertDialogHeader, AlertDialogTrigger } from "@/components/ui/alert-dialog"
-import { Label } from "@/components/ui/label"
-import { Input } from "@/components/ui/input"
-import { Icons } from "@/components/icons"
-import { InputOTP, InputOTPGroup, InputOTPSeparator, InputOTPSlot } from "@/components/ui/input-otp"
-import { validateEmail } from "@/lib/utils"
-import Image from "next/image"
-import Link from "next/link"
-import { forgotPassword, recoveryPassword } from "@/services/authService"
-import { RecoveryPasswordRequest } from "@/services/types"
+import { Button } from "@/components/ui/button";
+import {
+    AlertDialog,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { validateEmail } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { resetPassword } from "@/services/authService";
 
 interface UserRecoveryFormProps extends React.HTMLAttributes<HTMLDivElement> {
-    onOpenChange?: (open: boolean) => void
+    onOpenChange?: (open: boolean) => void;
 }
 
-export function UserRecoveryForm({ className, onOpenChange, ...props }: UserRecoveryFormProps) {
-    const [isLoading, setIsLoading] = React.useState<boolean>(false)
-    const [showOTP, setShowOTP] = React.useState<boolean>(false)
+export function UserRecoveryForm({
+    className,
+    onOpenChange,
+    ...props
+}: UserRecoveryFormProps) {
+    const [isLoading, setIsLoading] = React.useState<boolean>(false);
+    const [showOTP, setShowOTP] = React.useState<boolean>(false);
 
-    const [isFailRequest, setIsFailRequest] = React.useState<boolean>(false)
+    const [isFailRequest, setIsFailRequest] = React.useState<boolean>(false);
 
-    const [email, setEmail] = React.useState<string>('');
+    const [email, setEmail] = React.useState<string>("");
 
-    const [apiMessageResponse, setApiMessageResponse] = React.useState<{ title: string, description?: string }>({ title: '', description: '' });
+    const [apiMessageResponse, setApiMessageResponse] = React.useState<{
+        title: string;
+        description?: string;
+    }>({ title: "", description: "" });
 
     async function onSubmit(event: React.SyntheticEvent) {
-        event.preventDefault()
-        setIsLoading(true)
+        event.preventDefault();
+        setIsLoading(true);
 
         try {
-            const response = await forgotPassword(email)
-            const message = response.data.message
-            setApiMessageResponse(message)
-            setShowOTP(true)
-
+            const response = await resetPassword(email);
+            setApiMessageResponse({
+                title: "As instruções para redefinir sua senha foram enviadas para o seu e-mail.",
+                description:
+                    "Caso não encontre a mensagem em sua caixa de entrada, verifique o spam ou lixo eletrônico.",
+            });
+            setShowOTP(true);
         } catch (ex) {
-            console.log(ex)
-            setIsFailRequest(true)
+            console.log(ex);
+            setIsFailRequest(true);
         } finally {
-            setIsLoading(false)
+            setIsLoading(false);
         }
     }
 
@@ -50,51 +63,71 @@ export function UserRecoveryForm({ className, onOpenChange, ...props }: UserReco
         <AlertDialog onOpenChange={onOpenChange}>
             <AlertDialogTrigger asChild>
                 <Button
-                    variant='outline'
+                    variant="outline"
                     className="text-sm"
                     label="Esqueci minha senha"
                 />
             </AlertDialogTrigger>
-            <AlertDialogContent className={`${showOTP && "flex flex-col items-center justify-center"}`}>
+            <AlertDialogContent
+                className={`${showOTP && "flex flex-col items-center justify-center"}`}
+            >
                 <AlertDialogHeader>
-                    {
-                        !showOTP ?
-                            <div className="flex flex-col space-y-2 text-left mb-3 gap-2">
-                                <h1 className="text-2xl font-bold tracking-tight text-purple-700">
-                                    Esqueci minha senha
-                                </h1>
-                                <a className="text-grey-800 text-[16px]">
-                                    Insira o e-mail que você utilizou em seu cadastro, e vamos enviar instruções para redefinir sua senha.
-                                </a>
-                            </div>
-                            : !isFailRequest ?
-                                <div className="flex flex-col space-y-2 text-left items-center max-w-80 mb-3 gap-2">
-                                    <Image src='/PolygonOK.svg' width={120} height={120} alt="polygon" />
-                                    <h1 className="text-2xl font-bold tracking-tight text-center text-purple-700">
-                                        {apiMessageResponse?.title}
-                                    </h1>
-                                    <a className="text-grey-800 text-[16px]">
-                                        Enviamos um código de redefinição de senha para o seu e-mail <b>{email}</b>. <br />
-                                        Caso não encontre a mensagem em sua caixa de entrada, verifique o spam ou lixo eletrônico.
-                                    </a>
-                                </div>
-                                :
-                                <div className="flex flex-col space-y-2 text-left items-center max-w-80 mb-3 gap-2">
-                                    <Image src='/PolygonX.svg' width={120} height={120} alt="polygon" />
-                                    <h1 className="text-2xl font-bold tracking-tight text-center text-purple-700">
-                                        Ocorreu um erro durante o envio da mensagem
-                                    </h1>
-                                    <a className="text-grey-800 text-[16px] text-center">
-                                        Esse erro pode ter sido por conta da sua conexão com a internet ou algum problema no nosso servidor.
-                                        Por favor tente novamente em alguns minutos.
-                                    </a>
-                                </div>
-                    }
+                    {!showOTP ? (
+                        <div className="flex flex-col space-y-2 text-left mb-3 gap-2">
+                            <h1 className="text-2xl font-bold tracking-tight text-purple-700">
+                                Esqueci minha senha
+                            </h1>
+                            <a className="text-grey-800 text-[16px]">
+                                Insira o e-mail que você utilizou em seu
+                                cadastro, e vamos enviar instruções para
+                                redefinir sua senha.
+                            </a>
+                        </div>
+                    ) : !isFailRequest ? (
+                        <div className="flex flex-col space-y-2 text-left items-center max-w-80 mb-3 gap-2">
+                            <Image
+                                src="/PolygonOK.svg"
+                                width={120}
+                                height={120}
+                                alt="polygon"
+                            />
+                            <h1 className="text-2xl font-bold tracking-tight text-center text-purple-700">
+                                {apiMessageResponse?.title}
+                            </h1>
+                            <a className="text-grey-800 text-[16px]">
+                                Enviamos um código de redefinição de senha para
+                                o seu e-mail <b>{email}</b>. <br />
+                                Caso não encontre a mensagem em sua caixa de
+                                entrada, verifique o spam ou lixo eletrônico.
+                            </a>
+                        </div>
+                    ) : (
+                        <div className="flex flex-col space-y-2 text-left items-center max-w-80 mb-3 gap-2">
+                            <Image
+                                src="/PolygonX.svg"
+                                width={120}
+                                height={120}
+                                alt="polygon"
+                            />
+                            <h1 className="text-2xl font-bold tracking-tight text-center text-purple-700">
+                                Ocorreu um erro durante o envio da mensagem
+                            </h1>
+                            <a className="text-grey-800 text-[16px] text-center">
+                                Esse erro pode ter sido por conta da sua conexão
+                                com a internet ou algum problema no nosso
+                                servidor. Por favor tente novamente em alguns
+                                minutos.
+                            </a>
+                        </div>
+                    )}
                 </AlertDialogHeader>
 
                 <form className={`${showOTP && "hidden"}`} onSubmit={onSubmit}>
                     <div className="grid gap-4">
-                        <Label className="text-purple-700 font-semibold text-[16px]" htmlFor="email">
+                        <Label
+                            className="text-purple-700 font-semibold text-[16px]"
+                            htmlFor="email"
+                        >
                             E-mail
                         </Label>
                         <Input
@@ -114,20 +147,28 @@ export function UserRecoveryForm({ className, onOpenChange, ...props }: UserReco
                         />
                     </div>
                 </form>
-                <div className={`${!showOTP && 'hidden'} w-full flex flex-col gap-4 items-center max-w-80 justify-center mb-4`}>
+                <div
+                    className={`${!showOTP && "hidden"} w-full flex flex-col gap-4 items-center max-w-80 justify-center mb-4`}
+                >
                     <Link href={`/recovery/${email}`}>
-                        <Button disabled={!validateEmail(email)} label="Próximo" />
+                        <Button
+                            disabled={!validateEmail(email)}
+                            label="Próximo"
+                        />
                     </Link>
                 </div>
                 <AlertDialogFooter className="mt-4">
-                    <AlertDialogCancel onClick={() => {
-                        setEmail('')
-                        setShowOTP(false)
-                    }} className="w-full">
-                        {showOTP ? 'Voltar para o login' : 'Cancelar'}
+                    <AlertDialogCancel
+                        onClick={() => {
+                            setEmail("");
+                            setShowOTP(false);
+                        }}
+                        className="w-full"
+                    >
+                        {showOTP ? "Voltar para o login" : "Cancelar"}
                     </AlertDialogCancel>
                 </AlertDialogFooter>
             </AlertDialogContent>
         </AlertDialog>
-    )
+    );
 }
