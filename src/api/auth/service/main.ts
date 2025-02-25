00a1fffd399c96ba291d88fb083e7af377db3b44
Rfluid <ruy.vieiraneto@gmail.com>
2025-02-25 11:08:54 -0300

    feat(auth): add refresh token function

--- src/api/auth/service/main.ts
+++ src/api/auth/service/main.ts
@@ -4,11 +4,9 @@ import Cookie from "js-cookie";
 import { LoginRequest } from "@/api/auth/model/login-request.model";
 import { LoginResponse } from "@/api/auth/model/login-response.model";
 import { useChatStore } from "@/zustand-store/chatStore";
+import { useSessionStore } from "@/zustand-store/sessionStore";
 
-export const login = async ({
-    email,
-    password,
-}: LoginRequest): Promise<void> => {
+export async function login({ email, password }: LoginRequest): Promise<void> {
     try {
         const { token, refresh_token } = (
             await coreApi.post<LoginResponse>("/user/auth/token", {
@@ -25,7 +23,25 @@ export const login = async ({
         console.error("Erro ao fazer login", error);
         throw error;
     }
-};
+}
+
+export async function refreshToken(): Promise<void> {
+    try {
+        const { token, refresh_token } = (
+            await coreApi.post<LoginResponse>("/user/auth/refresh-token", {
+                refresh_token: useAuthStore.getState().tokens?.refresh_token,
+            })
+        ).data;
+
+        useAuthStore.getState().setTokens({ token, refresh_token });
+
+        Cookie.set("token", token ?? "");
+        Cookie.set("refresh_token", refresh_token ?? "");
+    } catch (error) {
+        console.error("Erro ao fazer login", error);
+        throw error;
+    }
+}
 
 // Old name is forgotPassword
 export function resetPassword(email: string) {
@@ -36,6 +52,7 @@ export const logout = (): void => {
     useAuthStore.getState().clearCurrentUser();
     useAuthStore.getState().clearTokens();
     useChatStore.getState().clearData();
+    useSessionStore.getState().clearChatSessions();
 
     Cookie.remove("token");
 };
